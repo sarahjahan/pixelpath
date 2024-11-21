4834944760d8ba4c12a34c35012611779e48b8c6
Sarah Jahan <sarah-jahan@live.com>
2024-11-21 14:51:23 -0500

    create and setup basic style for card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,14 @@
+import './Card.scss';
+
+function Card() {
+  return (
+    <>
+       <div className="card">
+            <h2>Title</h2>
+            <p>Genre</p>
+        </div>  
+    </>
+  );
+};
+
+export default Card;
