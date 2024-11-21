44b57389840181976bf94b988bb3a1d1d1d6e9e5
Sarah Jahan <sarah-jahan@live.com>
2024-11-21 14:52:12 -0500

    fix front-end server connection, add navbar and card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import SigninPage from './pages/SigninPage/SigninPage'
 import LibraryPage from '../src/pages/LibraryPage/LibraryPage'
-import GamesSearchPage from './pages/GamesSearchPage/GamesSearch'
+import GamesSearchPage from './pages/GamesSearchPage/GamesSearchPage'
+import GameDetailsPage from './pages/GameDetailsPage/GameDetailsPage'
 
-import { useEffect } from 'react'
+
+import { useEffect, useState } from 'react'
 import axios from "axios";
 import './App.css'
 import Button from './components/Button/Button'
+import Navbar from './components/Navbar/Navbar'
+import Card from './components/Card/Card'
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 function App() {
+  const [gamesList, setGamesList] = useState([])
+
+  console.log(gamesList);
+
 
   const getGamesAPI = async () => {
     try {
       const { data } = await axios.get(`${BASE_URL}/api/games`);
-      console.log("Client side confirmed");
+        setGamesList(data)
+        console.log(data)
+        console.log("Client side confirmed");
     } catch (error) {
       console.error("Error fetching games", error);
     }
@@ -25,14 +35,23 @@ function App() {
     getGamesAPI();
   }, []);
 
+  // if (gamesList.length === 0) {
+  //   return <div>no games found...</div>
+  // }
+
   return (
     <>
+      <Navbar />
       <Button onClick={getGamesAPI}/>
+      <Card />
+
+
 
       <BrowserRouter>
       <Routes>
         <Route path="/signin" element={<SigninPage />}/>
         <Route path="/library" element={<LibraryPage />} />
+        <Route path="/details" element={<GameDetailsPage />} />
         <Route path="/search" element={<GamesSearchPage />} />
         <Route path="/mood" />
         <Route path="*" element={<Navigate to="/" />} />
