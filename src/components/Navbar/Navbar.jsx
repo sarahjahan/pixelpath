c53bbb25a0f8b83259b3f297fd02a3c82d0c1d3c
Sarah Jahan <sarah-jahan@live.com>
2024-11-21 14:51:36 -0500

    create and setup basic styles for navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import './Navbar.scss';
+
+function Navbar() {
+    return(
+        <>
+        <div className="navbar">
+            <a href="/">Home</a>
+            <a href="/library">Library</a>
+            <a href="/search">Search</a>
+        </div>
+        </>
+    )
+}
+
+export default Navbar;
