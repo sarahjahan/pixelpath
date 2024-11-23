4b6547fcc196b8cc2e86b72effa74a336f8f1a7e
Sarah Jahan <sarah-jahan@live.com>
2024-11-23 00:25:19 -0500

    minor styling added for Card components

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,24 @@
 import './Card.scss';
+import delicon from '../../public/assets/icons/trash-2.svg';
+import editicon from '../../public/assets/icons/edit-2.svg';
 
-function Card({title, genre}) {
+
+function Card({title, genre, imgURL}) {
+
   return (
-    <div className="card">
+
+    <div className="card__container">
+      <img src={imgURL}/>
       <h2 className="card__title">{title}</h2>
       <p className="card__subtitle">{genre}</p>
+
+      <div className="card__action">
+        <img className="card__action--delete" src={delicon}/>
+        <img className="card__action--edit" src={editicon}/>
+      </div>
     </div>  
+
+
   );
 };
 
