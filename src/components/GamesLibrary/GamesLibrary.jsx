6b4bf4a5ec4b97f96da7fa4a0680899bd22d785b
Sarah Jahan <sarah-jahan@live.com>
2024-11-23 12:12:34 -0500

    add basic styling and UI elements to games library component

--- src/components/GamesLibrary/GamesLibrary.jsx
+++ src/components/GamesLibrary/GamesLibrary.jsx
@@ -1,17 +1,42 @@
 import Card from "../../components/Card/Card"
+import './GamesLibrary.scss'
+import { useState } from 'react'
 
 
 function GamesLibrary({gamesList}) {
+    const [searchKeyword, setSearchKeyword] = useState("");
+    const [filteredGames, setFilteredGames] = useState([]);
+    const [sortOrder, setSortOrder] = useState("asc");
+
+
     const [{ id, title, summary, coverArt, status, notes }] = gamesList
     console.log(gamesList)
     console.log(title)
 
     return(
-        <div className="gamesList__container">
-            {gamesList.length > 0 &&
-            gamesList.map((game, index) => (
-                <Card key={index} title={game.title} genre={game.summary} imgURL={game.coverArt}/>
-            ))}
+        <div>
+
+            <div className="gamesList__header">
+                <div className="gamesList__sorter">Sort</div>
+                <div className="gamesList__filter">Filter</div>
+                
+                <div className="gamesList__search">
+                    <input
+                        className="gamesList__searcher"
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Search..."
+                        onChange={(e) => setSearchKeyword(e.target.value)}/>
+                </div>
+            </div>
+            <div className="gamesList__container">
+                {gamesList.length > 0 &&
+                gamesList.map((game, index) => (
+                    <Card key={index} title={game.title} genre={game.summary} imgURL={game.coverArt}/>
+                ))}
+            </div>
+
         </div>
        
     )
