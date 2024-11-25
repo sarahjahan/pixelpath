56382c7125e28ab220fa989111d62eb6bd277344
Sarah Jahan <sarah-jahan@live.com>
2024-11-25 11:28:14 -0500

    refactor and fix backspace bug for search by keyword function

--- src/components/GamesLibrary/GamesLibrary.jsx
+++ src/components/GamesLibrary/GamesLibrary.jsx
@@ -7,58 +7,14 @@ import './GamesLibrary.scss'
 import { useState, useEffect } from 'react'
 
 
-function GamesLibrary({ gamesList, handleSort, delGame, addGame, getGamesLibrary, gamesAPIList, isSearchPage }) {
+function GamesLibrary({ gamesList, handleSort, delGame, addGame, getGamesLibrary, gamesAPIList, isSearchPage, isGameAdded }) {
     const [searchKeyword, setSearchKeyword] = useState("");
-    const [filteredGames, setFilteredGames] = useState([]);
 
-    // const [{ id, title, summary, coverArt, status, notes, rating }] = gamesList
-    // console.log(gamesList)
-    // console.log(title)
+    const filteredGames = (isSearchPage ? gamesAPIList : gamesList).filter((game) =>
+      game.title?.toLowerCase().includes(searchKeyword.toLowerCase())
+    );
 
-    useEffect(() => {
-        if (isSearchPage) {
-          setFilteredGames(gamesAPIList); 
-        } else {
-          setFilteredGames(gamesList);
-        }
-      }, [gamesList, gamesAPIList, isSearchPage]);
-
-
-      useEffect(() => {
-        if (searchKeyword) {
-          setFilteredGames((prevFilteredGames) =>
-            prevFilteredGames.filter((game) =>
-              game.title.toLowerCase().includes(searchKeyword.toLowerCase())
-            )
-          );
-        } else {
-          // If no search keyword, reset to the appropriate list
-          if (isSearchPage) {
-            setFilteredGames(gamesAPIList);
-            console.log(gamesAPIList)
-          } else {
-            setFilteredGames(gamesList);
-            console.log(gamesList)
-
-          }
-        }
-      }, [searchKeyword, gamesList, gamesAPIList, isSearchPage]);
-
-    // useEffect(() => {
-    //     const lowerCaseKey = searchKeyword.toLowerCase();
-    
-    //     setFilteredGames(
-    //         gamesList.filter((game) =>
-    //         Object.keys(game).filter((key) => key !== 'id').some((field) =>
-    //           game[field]?.toString().toLowerCase().includes(lowerCaseKey)
-    //         )
-    //       )
-    //     );
-    //   }, [searchKeyword, gamesList]);
-
-
-      if (!filteredGames) return <div>Loading games...</div>;
-
+    if (!gamesList && !gamesAPIList) return <div>Loading games...</div>;
 
 
     return(
@@ -78,7 +34,7 @@ function GamesLibrary({ gamesList, handleSort, delGame, addGame, getGamesLibrary
                 </div>
             </div>
             <div className="gamesList__container">
-                {filteredGames.length ==0? (<p className="result-message">No results found.</p>):
+                {filteredGames.length ===0? (<p className="result-message">No results found.</p>):
                 filteredGames.map((game, index) => (
                     <Card key={game.id}
                     game={game} 
@@ -87,7 +43,7 @@ function GamesLibrary({ gamesList, handleSort, delGame, addGame, getGamesLibrary
                     delGame={delGame}
                     addGame={addGame} 
                     getGamesLibrary={getGamesLibrary}
-                    isSearchPage={isSearchPage}  />
+                    isSearchPage={isSearchPage} isGameAdded={isGameAdded}  />
                 ))}
             </div>
         </div>
