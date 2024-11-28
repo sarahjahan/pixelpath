1cae13e57c387175855154249a9bde100cb688de
Sarah Jahan <sarah-jahan@live.com>
2024-11-28 14:05:25 -0500

    updates to navbar and discover button styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,9 +4,10 @@ function Navbar() {
     return(
         <>
         <div className="navbar">
-            <a href="/">Home</a>
+            <a className="navbar__logo" href="/">Home</a>
             <a href="/library">Library</a>
             <a href="/search">Search</a>
+            {/* <p className="site-header">Tracking your gaming journey, one pixel at a time ... </p> */}
         </div>
         </>
     )
