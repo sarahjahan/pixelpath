0144a186acfbbccbd97c4169a270560db085bc0c
Sarah Jahan <sarah-jahan@live.com>
2024-11-28 20:13:49 -0500

    move discover button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,19 @@
 import './Navbar.scss';
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+
+const navigate = useNavigate()
+
+
     return(
         <>
         <div className="navbar">
-            <a className="navbar__logo" href="/">Home</a>
             <a href="/library">Library</a>
-            <a href="/search">Search</a>
+            <a className="navbar__discover"href="/search">Discover New Games</a>
             {/* <p className="site-header">Tracking your gaming journey, one pixel at a time ... </p> */}
         </div>
+        
         </>
     )
 }
