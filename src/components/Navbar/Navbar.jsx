25675f8a0db97f67b52c886e6c641f204d1a6841
Sarah Jahan <sarah-jahan@live.com>
2024-11-29 09:29:50 -0500

    fix logo responsiveness / create div

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import './Navbar.scss';
 import { useNavigate } from "react-router-dom";
+import logo from "../../public/assets/controller.png"
 
 function Navbar() {
 
@@ -9,8 +10,15 @@ const navigate = useNavigate()
     return(
         <>
         <div className="navbar">
-            <a href="/library">Library</a>
-            <a className="navbar__discover"href="/search">Discover New Games</a>
+            <div className="navbar__menu">
+                <a href="/library">Library</a>
+                <a href="/search">Search</a>
+            </div>
+
+            <div className="navbar__logo">
+                <img onClick={() => navigate(`library`)} className="navbar__logo-img" src={logo} />
+            </div>
+            <a className="navbar__discover" href="/search">Discover New Games</a>
             {/* <p className="site-header">Tracking your gaming journey, one pixel at a time ... </p> */}
         </div>
         
