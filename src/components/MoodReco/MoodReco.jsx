e17255260e3c59437997538fbb7962012e10ccd1
Sarah Jahan <sarah-jahan@live.com>
2024-12-01 00:06:52 -0500

    add un-selected mood image styling

--- src/components/MoodReco/MoodReco.jsx
+++ src/components/MoodReco/MoodReco.jsx
@@ -7,9 +7,11 @@ import { useState } from 'react';
 
 
 function MoodReco({setMoodFilter}) {
+    const [selectedMood, setSelectedMood ] = useState(null)
     
     const handleMoodClick = (mood) => {
         setMoodFilter(mood); // Pass selected mood to GamesLibrary
+        setSelectedMood(mood);
       };
     
 
@@ -18,13 +20,13 @@ function MoodReco({setMoodFilter}) {
             <h2>How are you feeling?</h2>
             <h5>choose a game below based on your current mood</h5>
             <div className="moods__container">
-                <div className={`image__container`}>
+                <div className={`image__container ${selectedMood !== 'relaxed' ? 'unselected' : ''}`}>
                     <img className="Relaxed" src={chill} onClick={() => handleMoodClick('relaxed')}/>
                 </div>
-                <div className="image__container">
+                <div className={`image__container ${selectedMood !== 'happy' ? 'unselected' : ''}`}>
                     <img className="Adventurous" src={happy} onClick={() => handleMoodClick('happy')}/>
                 </div>
-                <div className="image__container">
+                <div className={`image__container ${selectedMood !== 'competitive' ? 'unselected' : ''}`}>
                     <img className="Competitive" src={angry} onClick={() => handleMoodClick('competitive')}/>
                 </div>
             </div>
