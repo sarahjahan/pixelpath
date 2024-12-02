acdf132289033e772aabe5ee709da3634df1904c
Sarah Jahan <sarah-jahan@live.com>
2024-12-01 20:01:37 -0500

    add labels to mood reco images

--- src/components/MoodReco/MoodReco.jsx
+++ src/components/MoodReco/MoodReco.jsx
@@ -18,16 +18,20 @@ function MoodReco({setMoodFilter, selectedMood, setSelectedMood}) {
     return (
         <div className="moods">
             <h2>How are you feeling?</h2>
-            <h5>choose a game below based on your current mood</h5>
+            <p className="tag-text">choose a game below based on your current mood</p>
             <div className="moods__container">
                 <div className={`image__container ${selectedMood !== 'relaxed' ? 'unselected' : ''}`}>
                     <img className="Relaxed" src={chill} onClick={() => handleMoodClick('relaxed')}/>
+                    <p className="tag-text">Relaxed</p>
                 </div>
                 <div className={`image__container ${selectedMood !== 'happy' ? 'unselected' : ''}`}>
                     <img className="Adventurous" src={happy} onClick={() => handleMoodClick('happy')}/>
+                    <p className="tag-text">Happy</p>
                 </div>
                 <div className={`image__container ${selectedMood !== 'competitive' ? 'unselected' : ''}`}>
                     <img className="Competitive" src={angry} onClick={() => handleMoodClick('competitive')}/>
+                    <p className="tag-text">Competitive</p>
+
                 </div>
             </div>
         </div>
